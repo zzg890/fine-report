b413ccc7e75d2c6fa252ce5e9fcbc17811409ec2
zzg <zzg890@outlook.com>
2020-06-29 13:13:13 +0800

    .

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-if [! -f "/usr/local/tomcat/bin/catalina.sh"]; then 
+if [ ! -f "/usr/local/tomcat/bin/catalina.sh" ]; then 
   mv /src/tomcat-linux/* /usr/local/tomcat
 fi
 /usr/local/tomcat/bin/catalina.sh run
